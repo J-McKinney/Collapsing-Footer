a8cd4daf81ee95dbb5a35c295518f7392055ca55
Jesse McKinney <jessemckinney513@gmail.com>
2020-07-22 12:22:25 -0400

    yay

---
+++ src/CollapsingFooter/CollapsingFooter.jsx
@@ -0,0 +1,27 @@
+import React, { useState } from "react";
+import Button from "react-bootstrap/Button";
+import Collapse from "react-bootstrap/Collapse";
+
+function CollapsingFooter() {
+  const [open, setOpen] = useState(false);
+  return (
+    <>
+      <Collapse in={open}>
+        <div id="example-collapse-text">
+          Anim pariatur cliche reprehenderit, enim eiusmod high life accusamus
+          terry richardson ad squid. Nihil anim keffiyeh helvetica, craft beer
+          labore wes anderson cred nesciunt sapiente ea proident.
+        </div>
+      </Collapse>
+      <Button
+        onClick={() => setOpen(!open)}
+        aria-controls="example-collapse-text"
+        aria-expanded={open}
+      >
+        Click Me
+      </Button>
+    </>
+  );
+}
+
+export default CollapsingFooter;
