97690b899b2e58fb8f2c1a45042ab50c48a5c887
Jesse McKinney <jessemckinney513@gmail.com>
2020-07-22 18:37:47 -0400

    yay

--- src/CollapsingFooter/CollapsingFooter.jsx
+++ src/CollapsingFooter/CollapsingFooter.jsx
@@ -27,60 +27,84 @@ class CollapsingFooter extends Component {
   render() {
     return (
       <>
-        <Container className="collapsingFooterContainer">
-          <Collapse in={this.state.open}>
-            <div id="example-collapse-text" className="collapsingContent">
-              <Row>
-                <Col>
-                  <ul>
-                    <li>
-                      <h5>The World Of React 1</h5>
-                    </li>
-                    <li>
-                      <h5>The World Of React 2</h5>
-                    </li>
-                    <li>
-                      <h5>The World Of React 3</h5>
-                    </li>
-                    <li>
-                      <h5>The World Of React 4</h5>
-                    </li>
-                  </ul>
-                </Col>
-                <Col>
-                  <ul>
-                    <li>
-                      <h5>The World Of React 5</h5>
-                    </li>
-                    <li>
-                      <h5>The World Of React 6</h5>
-                    </li>
-                    <li>
-                      <h5>The World Of React 7</h5>
-                    </li>
-                    <li>
-                      <h5>The World Of React 8</h5>
-                    </li>
-                  </ul>
-                </Col>
-              </Row>
-            </div>
-          </Collapse>
-        </Container>
+        <div className="footerWrapper">
+          <Container className="collapsingFooterContainer">
+            <Collapse in={this.state.open}>
+              <div id="example-collapse-text" className="collapsingContent">
+                <Row>
+                  <Col>
+                    <ul>
+                      <li>
+                        <h5>The World Of React 1</h5>
+                      </li>
+                      <li>
+                        <h5>The World Of React 2</h5>
+                      </li>
+                      <li>
+                        <h5>The World Of React 3</h5>
+                      </li>
+                      <li>
+                        <h5>The World Of React 4</h5>
+                      </li>
+                    </ul>
+                  </Col>
+                  <Col>
+                    <ul>
+                      <li>
+                        <h5>The World Of React 5</h5>
+                      </li>
+                      <li>
+                        <h5>The World Of React 6</h5>
+                      </li>
+                      <li>
+                        <h5>The World Of React 7</h5>
+                      </li>
+                      <li>
+                        <h5>The World Of React 8</h5>
+                      </li>
+                    </ul>
+                  </Col>
+                  <Col>
+                    <ul>
+                      <li>
+                        <h5>The World Of React 9</h5>
+                      </li>
+                      <li>
+                        <h5>The World Of React 10</h5>
+                      </li>
+                      <li>
+                        <h5>The World Of React 11</h5>
+                      </li>
+                      <li>
+                        <h5>The World Of React 12</h5>
+                      </li>
+                    </ul>
+                  </Col>
+                </Row>
+              </div>
+            </Collapse>
+          </Container>
 
-        <Button
-          className={this.state.changeNavBar ? "change" : ""}
-          id="collapsingFooterButton"
-          onClick={this.toggleCollapse}
-          aria-controls="example-collapse-text"
-          aria-expanded={this.state.open}
-        >
-          <div id="menuIconContainer">
-            <div className="bar1"></div>
-            <div className="bar2"></div>
-            <div className="bar3"></div>
-          </div>
-        </Button>
+          <Container>
+            <Row>
+              <Col>
+                <Button
+                  className={this.state.changeNavBar ? "change" : ""}
+                  id="collapsingFooterButton"
+                  onClick={this.toggleCollapse}
+                  aria-controls="example-collapse-text"
+                  aria-expanded={this.state.open}
+                >
+                  <div id="menuIconContainer">
+                    <div className="bar1"></div>
+                    <div className="bar2"></div>
+                    <div className="bar3"></div>
+                  </div>
+                </Button>
+              </Col>
+            </Row>
+          </Container>
+        </div>
       </>
     );
   }
